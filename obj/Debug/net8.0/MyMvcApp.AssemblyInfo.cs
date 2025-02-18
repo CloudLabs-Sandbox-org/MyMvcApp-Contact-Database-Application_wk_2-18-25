01597450dbcf7cffe51391c2ada97852360665dd
github_cloudlabsuser_1248 <github_cloudlabsuser_1248@cloudlabsaiuser.com>
2025-02-18 20:54:42 +0000

    More auto gen

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+676d5852fc3f9d0c635cb99a21941a7828b31160")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a117960c785f145d0633ef6cdd1be1d23b9b178")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
